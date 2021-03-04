bf81c67ee0aaa154b6120dadb9b70daa2e6cd79e
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-04 16:39:54 +0100

    Update main.cpp

--- WWO_01_16 - Średnia arytmetyczna/main.cpp
+++ WWO_01_16 - Średnia arytmetyczna/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  WWO_01_16 - Średnia arytmetyczna
-//
-//  Created by Kasia on 04/03/2021.
-//
+/*
+ Dla podanego ciąu liczb, znajdź taką, która jest najbliższą średniej arytmetycznej tych liczb. Jeśli jest kilka takich, wypisz największą.
 
+ Wejście
+ W pierwszym wierszu jedna liczba n określająca długość ciągu (nie więcej niż 1000)
+
+ W drugim wierszu n niewielkich liczb naturalnych.
+
+ Wyjście
+ Jedna liczba spełniająca kryteria zadania.
+ */
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <vector>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,p;
+    cin >> n;
+    int s = 0;
+    vector<int> v;
+    double sr,pom;
+    for(int i = 0;i < n;i++)
+    {
+        cin >> p;
+        v.push_back(p);
+        s += v[i];
+    }
+    sr = s/(double)n;
+    pom = abs(v[0] - sr);
+    int odp = v[0];
+    for(int i = 0;i < n;i++)
+    {
+        if(pom > abs(v[i] - sr)){
+            pom = abs(v[i] - sr);
+            odp = v[i];
+    }}
+    cout << odp << endl;
 }
